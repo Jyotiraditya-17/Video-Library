9a54bb97c2e31436203c266635b66dbda1e21f17
Ronak218 <jyotiadityahota@gmail.com>
2025-04-28 17:25:16 +0530

    Title Added

--- src/components/video-home.tsx
+++ src/components/video-home.tsx
@@ -8,12 +8,16 @@ export function VideoHome() {
             backgroundSize: "cover",
             backgroundPosition: "center",
             minHeight: "100vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
         }}>
-            <Link className="btn btn-dark mx-2" to='/user-login'> User Login </Link>
-            <Link className="btn btn-warning" to='/admin-login'> Admin Login </Link>
+
+            <div>
+                <h1 className="fw-bold text-dark"> VidNest </h1>
+            </div>
+            
+            <div className="d-flex justify-content-center align-items-center" style={{marginTop:'200px'}}>
+                <Link className="btn btn-dark mx-2" to='/user-login'> User Login </Link>
+                <Link className="btn btn-warning" to='/admin-login'> Admin Login </Link>
+            </div>
         </div>
     )
 }
